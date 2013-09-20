826b81c094aa20af158e3f497a7708a18eb7c7c7
Raphael Randschau <nicolai86@me.com>
2013-09-20 17:54:22 +0200

    move echo methods into separate func

--- src/echoserver/core.clj
+++ src/echoserver/core.clj
@@ -9,17 +9,20 @@
 (defn generate-input-display [input]
   (str "< " input "\n"))
 
-(defn echo [in out]
-  (let [reader (BufferedReader. (InputStreamReader. in))
-          writer (OutputStreamWriter. out)]
-      (loop []
-        (let [input (. reader readLine)]
+(defn echo [reader writer]
+  (let [input (. reader readLine)]
           (. writer write (generate-output input))
           (. writer flush)
           (print (generate-input-display input))
-          (flush))
+          (flush)))
+
+(defn setup-echo [in out]
+  (let [reader (BufferedReader. (InputStreamReader. in))
+          writer (OutputStreamWriter. out)]
+      (loop []
+        (echo reader writer)
         (recur))))
 
 (defn main []
   (println "Hello, World!")
-  (clojure.contrib.server-socket/create-server 8080 echo))
+  (clojure.contrib.server-socket/create-server 8080 setup-echo))
