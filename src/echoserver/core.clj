bc493481b847c67e53d9c31871ad37ed730bcf62
Raphael Randschau <nicolai86@me.com>
2013-09-20 12:14:24 +0200

    initial commit

---
+++ src/echoserver/core.clj
@@ -0,0 +1,19 @@
+(ns echoserver.core
+  (:require clojure.contrib.server-socket))
+
+(import '[java.io BufferedReader InputStreamReader OutputStreamWriter])
+
+(defn echo-server []
+  (letfn [(echo [in out]
+                    (binding [*in* (BufferedReader. (InputStreamReader. in))
+                              *out* (OutputStreamWriter. out)]
+                      (loop []
+                        (let [input (read-line)]
+                          (print input)
+                          (flush))
+                        (recur))))]
+    (clojure.contrib.server-socket/create-server 8080 echo)))
+
+(defn main []
+  (println "Hello, World!")
+  (echo-server))
---
+++ test/echoserver/core_test.clj
@@ -0,0 +1,7 @@
+(ns echoserver.core-test
+  (:require [clojure.test :refer :all]
+            [echoserver.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
